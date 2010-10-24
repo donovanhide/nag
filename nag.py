c5a6ade5554fea4b72868df2ea93b6d3e49e8a45
Donovan Hide <donny@sur.ly>
2010-10-24 23:20:58 +0100

    Added argparse Refactored to demonstrate OOP!

--- nag.py
+++ nag.py
@@ -1,108 +1,103 @@
 #!/usr/bin/env python
 
-import sys, os, fileinput, getopt
+import os
+import argparse
+import StringIO
 
-home = os.getenv("HOME")
-filename = home + "/.nag"
-test = open(filename, "a")
-test.close()
-size = os.path.getsize(filename)
-number_of_lines = len(open(filename).readlines())
+class NagItem():
+    
+    def __init__(self,priority,task):
+        self.priority = int(priority)
+        self.task = task
+        
+    def __cmp__(self,other):
+        return cmp(self.priority,other.priority) or cmp(self.task,other.task)
 
-def usage():
-    print "usage: nag [-acdls] "
-    table = {"-a": "add an item to your list", "-c": "clear your list", "-d [n]": "remove item [n] from your list", "-l": "show the contents of your list", "-s": "search your list"}
-    for command, explanation in table.items():
-        print "{0:10}  {1:10}".format(command, explanation)
+    def __str__(self):
+        return '%s\t%s' % (self.priority,self.task)
+    
+    def __repr__(self):
+        return self.__str__()
 
-def error():
-    print "There's nothing to do!\nUse 'nag -a' to add a new item to your list."
+class Nag():
+    
+    def __init__(self):
+        self.parse()
 
-def add():
-    if len(sys.argv) == 2:
-        print "Nothing to add! See nag -h for help."
-    else:
-        new_item = " ".join(sys.argv[2:])
-        with open(filename, "a") as f:
-            f.write(new_item + "\n")
-
-def list():
-    if size == 0:
-        error()
-    else:
-        for line in fileinput.input(filename):
-            print fileinput.lineno(), line,
-        print "---\nYou have", number_of_lines, "items in", filename
-
-def clear():
-    print "Are you sure you want to clear your nag list? (y or n)"
-    answer = raw_input("> ")
-    if answer == "y":
-        with open(filename, "w") as f:
-            f.truncate()
-            print "List cleared!",
-    else:
-        exit()
-
-def first_line():
-    if size == 0:
-        error()
-    else:
-        with open(filename) as f:
-            print "Next item: " + f.readline(),
-
-def delete():
-    line_to_delete = int(sys.argv[2])
-    if line_to_delete > number_of_lines:
-        print "Item", line_to_delete, "doesn't exist!", "You only have ", number_of_lines, "item(s)."
-    else:
-        lines = file(filename, "r+").readlines()
-        del lines[line_to_delete -1]
-        open(filename, 'w').writelines(lines)
-        print "Item", line_to_delete , "deleted!"
-
-def searcher():
-    if len(sys.argv) == 2:
-        print "Nothing to search for!"
-    else:
-        search_term = "".join(sys.argv[2])
-        line_num = 0
-        f = open(filename)
-        for line in f.readlines():
-            line_num += 1
-            if search_term in line:
-                print line_num, line,
-                f.close()
-                break
-            else:
-                print search_term, "not found.",
-
-def main(argv):
-    if len(sys.argv) == 1:
-        first_line()
-    else:
+    def __iter__(self):
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "acd:hls", ["add", "clear", "delete=", "help", "list", "search"])
-        except getopt.GetoptError, err:
-            print str(err) 
-            usage()
-            sys.exit(2)
+            with open(self.args.filename,"r") as f:
+                return (NagItem(*line.strip().split('\t',1)) for line in f.readlines())
+        except IOError:
+            return iter([])
+            
+    def __str__(self):
+        string = StringIO.StringIO()
+        string.write("Line:\tPriority:\tTask:\n")
+        for line_number,item in enumerate(self):
+            if not hasattr(self.args,'term') or self.args.term.lower() in item.task.lower():
+                string.write("%s\t%s\t\t%s\n" %(line_number+1,item.priority,item.task))
+        return string.getvalue()
+    
+    def __save__(self,new_content):
+        with open(self.args.filename, "w") as f:
+            for item in sorted(new_content):
+                f.write('%s\n'%item)
+    
+    @property
+    def items(self):
+        return [item for item in self]
 
-        for opt, arg in opts:
-            if opt in ("-h", "--help"):
-                usage()    
-            elif opt in ("-l", "--list"):
-                list()
-            elif opt in ("-a", "--add"):
-                add()
-            elif opt in ("-c", "--clear"):
-                clear()
-            elif opt in ("-s", "--search"):
-                searcher()
-            elif opt in ("-d", "--delete"):
-                delete()
-            else:
-                sys.exit(2)
+    def list(self):
+        print self
+
+    def add(self):
+        self.__save__(self.items+[NagItem(self.args.priority,self.args.item)])
+
+    def clear(self):
+        answer = raw_input("Are you sure you want to clear your nag list? (y or n)")
+        if answer == "y":
+            self.__save__([])
+            print "List cleared!"
+
+    def delete(self):
+        current = self.items
+        try:
+            del current[self.args.line-1]
+            self.__save__(current)
+        except IndexError:
+            print "Item: %s doesn't exist! You only have %s items " % (self.args.line,len(current))
+
+    def parse(self):
+        parser = argparse.ArgumentParser(description='Yet another TODO list!')
+        subparsers = parser.add_subparsers(help='commands')
+
+        list_parser = subparsers.add_parser('list', help='List items')
+        list_parser.set_defaults(func=self.list)
+
+        add_parser = subparsers.add_parser('add', help="Add an item")
+        add_parser.add_argument('--item','-i',help="Item to add", required=True)
+        add_parser.add_argument('--priority','-p',help="Priority of item [default=%(default)s]",type=int,default=1)
+        add_parser.set_defaults(func=self.add)
+
+        clear_parser = subparsers.add_parser('clear', help='Clear all items')
+        clear_parser.set_defaults(func=self.clear)
+
+        search_parser = subparsers.add_parser('search',help='Search for an item')
+        search_parser.add_argument('--term','-t',help='Term to search for',required=True)
+        search_parser.set_defaults(func=self.list)
+
+        delete_parser = subparsers.add_parser('delete',help='Delete an item')
+        delete_parser.add_argument('--line','-l',help='Line number of item to delete',required=True,type=int)
+        delete_parser.set_defaults(func=self.delete)
+
+        parser.add_argument('-f','--file',help='TODO list to process [default=%default]',
+                                          dest='filename',
+                                          default=os.path.join(os.getenv('HOME'),'.nag'))
+        parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+
+        self.args = parser.parse_args()
+        self.args.func()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    Nag()
